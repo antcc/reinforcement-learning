0271dafd0255f4886e79d6eb1c2d1f6b7ccce650
antcc <antoniocoin07@gmail.com>
2021-05-08 20:25:18 +0200

    Update

---
+++ agent.py
@@ -0,0 +1,109 @@
+from viewer import Viewer
+import gym
+import numpy as np
+from gym.spaces import Discrete, Box
+
+class myEnv(gym.Env):
+    metadata = {"render.modes": ["human", "rgb_array"], "video.frames_per_second": 50}
+
+    def __init__(self, target_coords=[250, 303], mode="hard"):
+        self.state_dim = 7
+        self.dt = 0.1  # refresh rate
+
+        self.arm1_long = 100
+        self.arm2_long = 100
+        self.arm1_ang = 0
+        self.arm2_ang = 0
+        self.arm1_coords = np.array([0, 0])
+        self.arm2_coords = np.array([0, 0])
+
+        self.viewer = None
+        self.viewer_xy = (400, 400)
+        self.got_target = False
+        self.mouse_in = np.array([False])
+        self.target_width = 15
+
+        self.action_space = Discrete(3 * 3)  #
+        # observation:
+        self.observation_space = Box(
+            low=-1e10, high=1e10, shape=(self.state_dim,), dtype=np.float32
+        )
+        self.observation_space.n = self.state_dim
+        self.mode = mode
+
+        self.target_coords = np.array(target_coords)
+        self.target_coords_init = self.target_coords.copy()
+        self.center_coords = np.array(self.viewer_xy) / 2
+        self.effective_r = np.mean([self.target_width, np.sqrt(2*self.target_width)])
+
+    def render(self):
+        if self.viewer is None:
+            self.viewer = Viewer(
+                *self.viewer_xy,
+                self.arm1_coords,
+                self.arm2_coords,
+                self.arm1_ang,
+                self.arm2_ang,
+                self.target_coords,
+                self.target_width,
+                self.mouse_in
+            )
+        self.viewer.render(
+            self.arm1_coords, self.arm2_coords, self.arm1_ang, self.arm2_ang
+        )
+
+    def reset(self):
+        self.got_target = False
+
+        if self.mode == "hard":
+            pxy = np.random.randint(low = -self.viewer_xy[0] + self.center_coords[0],
+                                    high = self.viewer_xy[0] - self.center_coords[0],
+                                    size = 2)
+            self.target_coords[:] = np.clip(pxy, 100, 300)
+        else:
+            #TODO: mover el brazo a una posición aleatoria
+            self.arm1_ang = 0
+            self.arm2_ang = 0
+            self.arm1_coords = np.array([0, 0])
+            self.arm2_coords = np.array([0, 0])
+            self.target_coords[:] = self.target_coords_init
+
+        s = self._get_state()
+        return s  # observation
+
+    def _get_state(self):
+        """
+        Returns arm coordinates
+        """
+        return np.array(
+            [*self.arm1_coords, *self.arm2_coords, *self.target_coords])
+
+    def step(self, act):
+        action1 = act // 3 - 1
+        action2 = act % 3 - 1
+
+        self.arm1_ang += action1 * self.dt
+        self.arm1_ang %= np.pi * 2
+        self.arm2_ang += action2 * self.dt
+        self.arm2_ang %= np.pi * 2
+
+        self.arm1_coords = self.center_coords.copy()
+        self.arm1_coords[0] += self.arm1_long * np.cos(self.arm1_ang)
+        self.arm1_coords[1] += self.arm1_long * np.sin(self.arm1_ang)
+
+        self.arm2_coords = self.arm1_coords.copy()
+        self.arm2_coords[0] += self.arm2_long * np.cos(self.arm1_ang + self.arm2_ang)
+        self.arm2_coords[1] += self.arm2_long * np.sin(self.arm1_ang + self.arm2_ang)
+
+        s = self._get_state()
+
+        # Euclidean distance between
+        r = np.linalg.norm(self.arm2_coords - self.target_coords)
+
+        self.got_target = r < self.effective_r
+
+        return s, -r/self.viewer_xy[0], self.got_target, {}
+
+    def close(self):
+        if self.viewer:
+            self.viewer.close()
--- main.py
+++ main.py
@@ -1,29 +1,30 @@
-from numpy.core.fromnumeric import reshape
-from viewer import *
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.optimizers import Adam
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+import numpy as np
+from agent import myEnv as Agent
 import time
-import pandas as pd
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.models import Sequential
+
 np.random.seed(1)
 
 
 def build_q_nnet(n_vars, n_actions, alpha):
     model = Sequential()
-    model.add(Dense(5,input_shape=(n_vars,),
-                   bias_initializer="RandomNormal",
-                   activation="relu"))
+    model.add(Dense(10, input_shape=(n_vars,),
+                    bias_initializer="RandomNormal",
+                    activation="relu"))
     model.add(Dense(n_actions))
-    model.compile(loss='mse',optimizer=Adam(lr=alpha))
+    model.compile(loss='mse', optimizer=Adam(learning_rate=alpha))
     return model
 
 
 def choose_action(state, nnet, epsilon):
-    # This is how to choose an action
-    # devuelve 0 o 1: 0 si left, 1 si right
-
-    state_actions = nnet.predict(state.reshape(1,-1))[0]
-    if (np.random.uniform() < epsilon) or ((state_actions == 0).all()):  # act non-greedy or state-action have no value
+    state_actions = nnet.predict(state.reshape(1, -1))[0]
+    # act non-greedy or state-action have no value
+    if np.random.uniform() < epsilon or (state_actions == 0).all():
         act = np.random.randint(len(state_actions))
     else:   # act greedy
         act = np.argmax(state_actions)
@@ -31,57 +32,70 @@ def choose_action(state, nnet, epsilon):
 
 
 def rl(env, epsilon, alpha, gamma, n_episodes,
-       sleep_time, actions, vis=True):
-    
+       sleep_time=0.01, actions=8, vis=True, max_steps=1000,
+       decay=0.9, min_epsilon=0.01):
+
     state = env._get_state()
     q_nnet = build_q_nnet(len(state), actions, alpha)
-    
+    steps_hist = []
+    rewards_hist = []
+
     for episode in range(n_episodes):
-        step_counter = 0
         S = env.reset()
         done = False
+        total_reward = 0
 
-        while not done:
+        for step in range(max_steps):
             act = choose_action(S, q_nnet, epsilon)
-            S_, R, done, _ = env.step(act)  # take action & get next state and reward
-            
+            # take action & get next state and reward
+            S_, R, done, _ = env.step(act)
+            total_reward += R
+
             if not done:
-                q_target = R + gamma*q_nnet.predict(S_.reshape(1,-1)).max() # next state is not terminal
+                # next state is not terminal
+                q_target = R + gamma*q_nnet.predict(S_.reshape(1, -1)).max()
             else:
                 q_target = R            # next state is terminal
-                interaction = 'Episodio %s: total steps = %s' % (episode+1, step_counter)
-                print('\r{}'.format(interaction), end='')
+                print('Episodio %s: total steps = %s, total reward = %.2f' % (
+                    episode + 1, step + 1, total_reward))
 
-            target_vector = q_nnet.predict(S.reshape(1,-1))
+            target_vector = q_nnet.predict(S.reshape(1, -1))
             target_vector[0][act] = q_target
-            q_nnet.fit(S.reshape(1,-1), target_vector, verbose=0, epochs=1) # update
-            
+            q_nnet.fit(S.reshape(1, -1), target_vector,
+                       verbose=0, epochs=1)  # update
+
             S = S_  # move to next state
-            
+
             if vis and not done:
                 env.render()
                 time.sleep(sleep_time)
-            step_counter += 1
-        
-    return q_nnet
 
-env = myEnv()
+            if done:
+                steps_hist.append(step + 1)
+                rewards_hist.append(step + 1)
+                break
+            elif step + 1 == max_steps:
+                print(f"Episodio {episode + 1}: no alcanza tras {step + 1} pasos")
 
-q_nnet = rl(env, 
-            epsilon = 0.4, 
-            alpha = 0.01,
-            gamma = 0.9,
-            n_episodes = 13,
-            sleep_time = 0.01, 
-            actions = 8,
-            vis = True)
+        if epsilon > min_epsilon:
+            epsilon *= decay
+
+    return q_nnet, episode + 1, steps_hist, rewards_hist
 
 
-# while True:
-#     env.reset()
-#     env.render()
-#     accion = int(input("acción (0 a 8): "))
-#     nsteps = int(input("nsteps (>0):    "))
-#     for i in range(nsteps):
-#         env.step(accion)
-#         env.render()
+def main():
+    env = Agent(mode='easy')
+    q_nnet, _ = rl(
+        env,
+        epsilon=0.1,
+        alpha=0.01,
+        gamma=1.0,
+        n_episodes=10,
+        vis=True,
+        decay=0.9
+        max_steps=1000,
+    )
+
+
+if __name__ == '__main__':
+    main()
